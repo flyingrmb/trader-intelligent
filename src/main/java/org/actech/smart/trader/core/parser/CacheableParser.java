184c5628e48c5d69216ebb1b13a061816a983d5e
paul <wechat_pw@163.com>
2018-03-17 11:59:03 +0800

    增加同步股票历史交易数据服务-修复bug

--- src/main/java/org/actech/smart/trader/core/parser/CacheableParser.java
+++ src/main/java/org/actech/smart/trader/core/parser/CacheableParser.java
@@ -5,6 +5,7 @@ import org.actech.smart.trader.core.net.NetworkResourceCache;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 /**
  * Created by paul on 2018/3/11.
@@ -15,7 +16,7 @@ public abstract class CacheableParser<T> implements HtmlParser, Cacheable {
 
     @Override
     public boolean shouldParse(String url) {
-        Class<T> entityClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        Class<T> entityClass = (Class<T>)(getParameterizedType(getClass())).getActualTypeArguments()[0];
         return shouldParse(cache.get(url, entityClass));
     }
 
@@ -23,8 +24,16 @@ public abstract class CacheableParser<T> implements HtmlParser, Cacheable {
 
     @Override
     public void parse(String url, Object parameter) {
-        Class<T> entityClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        Class<T> entityClass = (Class<T>)(getParameterizedType(getClass())).getActualTypeArguments()[0];
         parse(cache.get(url, entityClass), parameter);
+    }
+
+    private ParameterizedType getParameterizedType(Class clazz) {
+       Type type = clazz.getGenericSuperclass();
+       if (type instanceof ParameterizedType)
+           return (ParameterizedType)type;
+
+       return getParameterizedType((Class)type);
     }
 
     public abstract void parse(T htmlPage, Object dateStr);
